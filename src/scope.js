267a9ee18ed226aa35c2f7294e8b84ad6355836f
Igor Koba <IgorKoba@igors-mbp.teaminternational.com>
2016-12-05 12:46:30 +0200

    merge conflicts

--- src/scope.js
+++ src/scope.js
@@ -1,4 +1,18 @@
 function Scope() {
+	this.$watchers = [];
 }
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+	this.$watchers.push({
+		watchFn: watchFn,
+		listenerFn: listenerFn
+	});
+};
+
+Scope.prototype.$digest = function() {
+	for (var i = 0; i < this.$watchers.length; i++) {
+		this.$watchers[i].listenerFn();
+	}
+};
 
 module.exports = Scope;
